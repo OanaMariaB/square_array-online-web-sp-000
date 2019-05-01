9822847bd42d37e25652b4b14d4e31253d41a167
Oana Berentan <oana.berentan@yahoo.com>
2019-05-01 21:46:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,10 +3,3 @@ def square_array(array)
   array.each{|i| new_array.push(i**2)}
   return new_array
 end
-
-
-def square_array_version_two(array)
-  array.collect do|n|
-    puts n ** 2
-    n ** 2
-end
